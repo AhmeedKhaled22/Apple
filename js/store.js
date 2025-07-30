d7227de689ffa7bee6bc9b709cd42872fde2f525
AhmeedKhaled22 <wascsacaca@gmail.com>
2025-07-30 10:00:26 +0300

    first commit

---
+++ js/store.js
@@ -0,0 +1,9 @@
+let img=document.querySelector('.img')
+let continer=document.querySelector('.container')
+function phones(phone){
+img.src=phone;
+}
+function colors(color){
+  continer.style.background=color;
+}
+
